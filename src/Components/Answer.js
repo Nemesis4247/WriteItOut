ee88dc9eb040b38d2cafb3b46c8a500e19928e0f
relaxedcoder <devjit.menghani@gmail.com>
2020-10-15 23:20:44 +0530

    Added Answer component(hard-coded values)

---
+++ src/Components/Answer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from './Answer.module.css'
+import Profile from './Signin/chico.png'
+
+export default function Answer() {
+    return (
+        <div id={styles.container}>
+            <div id={styles.profile}>
+                <img src={Profile} id={styles.photo} />
+                <div id={styles.bio}>
+                    <h5>FirstName LastName</h5>
+                    <p>This is the bio</p>
+                </div>
+            </div>
+            <div id={styles.body}>
+                This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer This is the answer
+            </div>
+        </div>
+    )
+}
