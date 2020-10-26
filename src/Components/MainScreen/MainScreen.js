ea1df9898d6e4cd5ba9609dc35da1f90126589cd
HemantYadav <hy27.1999@gmail.com>
2020-10-26 09:40:09 +0530

    added like functionality on question

--- src/Components/MainScreen/MainScreen.js
+++ src/Components/MainScreen/MainScreen.js
@@ -51,45 +51,50 @@ class MainScreen extends React.Component {
       searchQuestionByString: "",
       searchQuestionByTag: "",
       checked: false,
-      questionsList: [
-        {
-          question_id: 1,
-          username: "Hemant Yadav",
-          description: "Mobile and Web Developer",
-          question: "When we see something, it behaves differently, and when we do not see it, it behave differently. Is the world very mysterious? What do you know about Photon of Double Slit?What is one piece of simple advice that actually changed your life?",
-          upvotes: 5
-        },
-        {
-          question_id: 2,
-          username: "Ishaan Pandey",
-          description: "Quantum Computing Enthusiastic",
-          question: "What is the significance of major project in 4th year of engineering if one doesn't want to pursue an engineering job. Like what should one do in the major project if one just wants to get the engineering degree?",
-          upvotes: 3
-        },
-        {
-          question_id: 3,
-          username: "Devjit Meghani",
-          description: "BLockchain Enthusiastic",
-          question: "What is one piece of simple advice that actually changed your life?",
-          upvotes: 7
-        },
-        {
-          question_id: 4,
-          username: "Ashish Ucheniya",
-          description: "Mobile and Web Developer",
-          question: "How do I write some Python code that simulations the toss of a coin (0=heads and 1= tails)? How do I modify this code so that the user is asked if they want to toss again (Y/N)?",
-          upvotes: 5
-        },
-        {
-          question_id: 5,
-          username: "Ankit Aharwal",
-          description: "Mobile and Web Developer",
-          question: "Can TensorFlow lite C++ library be used to program a part of an Operating System since TensorFlow lite C++ library is used in programming microcontrollers?",
-          upvotes: 10
-        },
-      ]
+      questionsList: []
 		}
 	}
+
+  componentDidMount(){
+    fetch('http://127.0.0.1:3001/getQuestionsList')
+    .then(response => response.json())
+    .then(data => {
+        if (data.status) {
+
+                fetch('http://127.0.0.1:3001/likedQuestions',{
+            			method: 'post',
+            			headers: {'Content-Type':'application/json'},
+            			body:JSON.stringify({
+                    userid: this.state.enr_no
+            			})
+            		})
+                .then(response => response.json())
+                .then(data1 => {
+                    if (data1.status) {
+                        var que_list = data.data.message;
+                        var likedQuetions = data1.data.message;
+                        for (var i = 0; i < que_list.length; i++) {
+                          que_list[i]["liked"] = likedQuestions.includes(que_list[i]["queid"])
+                        }
+                        this.setState({questionsList: que_list})
+                    }
+                    else{
+                      alert("Error: " + data1.data.message)
+                    }
+                })
+                .catch(err1 => {
+                    alert("Error: " + err1)
+                });
+
+        }
+        else{
+            alert("Error: " + data.data.message)
+        }
+    })
+    .catch(err => {
+        alert("Error: " + err)
+    });
+  }
 
   onNameChange = (event) => {
 		this.setState({ name: event.target.value })
--- src/Components/MainScreen/Questions/Question.js
+++ src/Components/MainScreen/Questions/Question.js
@@ -23,10 +23,35 @@ class Question extends React.Component {
 			name: this.props.username,
 			description: this.props.description,
       upvotes: this.props.upvotes,
-      imageURL: "",
+      imageURL: this.props.imagepath,
+      datetime: this.state.datetime,
       liked: this.props.liked
 		}
 	}
+
+  likeToggle = () => {
+    fetch('http://127.0.0.1:3001/toggleLikeQuestion',{
+      method: 'post',
+      headers: {'Content-Type':'application/json'},
+      body:JSON.stringify({
+        userid: this.state.userid,
+        queid: this.state.question_id,
+        like: !this.state.liked
+      })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if(data.status) {
+            this.setState({liked: !this.state.liked})
+        }
+        else{
+          alert("Error: " + data.data.message)
+        }
+    })
+    .catch(err => {
+        alert("Error: " + err)
+    });
+  }
 
 render(){
 	return (
@@ -34,11 +59,12 @@ render(){
 			<div className="w-100 mv2 bg-moon-gray v-mid br3 ph2">
           <div className="dt">
 						<div className="dtc pa2 v-mid">
-							<img className="dtc fc h3 w3" src={ profile }/>
+							<img className="dtc fc h3 w3" src={ this.state.imageURL }/>
 						</div>
 						<header className="dtc tl">
 							<p className="f3 b v-top">{ this.state.name }</p>
 							<p className="f6 v-top">{ this.state.description }</p>
+              <p className="f6 v-top">{ this.state.datetime }</p>
 						</header>
 					</div>
           <hr/>
@@ -49,8 +75,8 @@ render(){
 					<div className="tl">
 	          {
 	            this.state.liked === true?
-	            <img className="dib h2 w2 ma1" src={ u_filled }/>:
-	            <img className="dib h2 w2" src={ u_blank }/>
+	            <img className="dib h2 w2 ma1" src={ u_filled } onClick={ this.likeToggle }/>:
+	            <img className="dib h2 w2" src={ u_blank } onClick={this.likeToggle}/>
 	          }
 	          <p className="dib f3">{ this.state.upvotes }</p>
 					</div>
