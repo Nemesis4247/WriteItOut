78727051f1105fb6fff2acf8e20ab3cc8f2f3be3
relaxedcoder <devjit.menghani@gmail.com>
2020-10-30 12:01:12 +0530

    Add answer functionality.

--- src/Components/QuestionDetail/QuesAnsComment.js
+++ src/Components/QuestionDetail/QuesAnsComment.js
@@ -6,6 +6,7 @@ import Radium, { StyleRoot } from 'radium';
 import { zoomIn } from 'react-animations';
 import { fadeOut } from 'react-animations';
 import profile_image from '../Signin/profile.svg';
+import Profile from '../Signin/chico.png';
 
 import QuestionHeading from './QuestionHeading/QuestionHeading'
 import Answer from './Answer/Answer'
@@ -26,6 +27,25 @@ export default function QuesAnsComment(props) {
     const { id } = useParams()
 
     const [data, setData] = useState(null)
+    const [answer, setAnswer] = useState('')
+
+    function addAnswer() {
+        fetch('http://127.0.0.1:3001/answer', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: {
+                ans: answer,
+                queid: id,
+                userid: props.data.userid
+            }
+        })
+            .then(response => {
+                if (response.ok) alert('Answer added!')
+                else alert('Error!')
+            })
+    }
 
     useEffect(() => {
         const fetcher = () => {
@@ -45,6 +65,26 @@ export default function QuesAnsComment(props) {
                 <div className="dtc w-70 bg-near-white v-top h-100" >
                     <div id={styles.container}>
                         {data && <QuestionHeading questionHeading={data.que} />}
+                        <div id={styles.answer}>
+                            <img src={Profile} id={styles.answerPhoto} />
+                            <textarea
+                                id={styles.answerBox}
+                                placeholder='Add an anwser...'
+                                onChange={e => {
+                                    setAnswer(e.target.value)
+                                    e.target.style.height = 'inherit'
+                                    e.target.style.height = e.target.scrollHeight + 'px'
+                                }}
+                                value={answer}
+                                onClick={addAnswer}
+                            />
+                            <button
+                                id={styles.addAnswer}
+                                onClick={() => { }}
+                            >
+                                Add answer
+                            </button>
+                        </div>
                         {data && data.answers.map(answer => {
                             return <Answer
                                 ansid={answer.ansid}
